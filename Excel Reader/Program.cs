96880432d340f94a0854e04baf6a74d24bc5caf2
Arad <AradAryanBusiness@gmail.com>
2020-12-16 20:38:17 +0330

    Add project files.

---
+++ Excel Reader/Program.cs
@@ -0,0 +1,35 @@
+﻿using OfficeOpenXml;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excel_Reader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string fileName = @"C:\Users\javad\Desktop\accounts.xlsx";
+            StreamReader streamReader = new StreamReader(fileName);
+            using (var excel = new ExcelPackage(streamReader.BaseStream))
+            {
+                var workbook = excel.Workbook;
+                var worksheet = excel.Workbook.Worksheets[1];
+                for (int RowIndex = 2; RowIndex <= worksheet.Dimension.End.Row; RowIndex++)
+                {
+                    try
+                    {
+                        var temp = worksheet.Cells[RowIndex, 1].Value.ToString();
+                    }
+                    catch (Exception)
+                    {
+                        continue;
+                    }
+                }
+            }
+        }
+    }
+}
